144c50acf1ff1d439c693ac2e2f7dc24eafdc193
Tushyap <baritushyap9120@gmail.com>
2024-02-02 15:31:13 +0530

    v17

--- portfolio/src/Components/NavbarComponent.jsx
+++ portfolio/src/Components/NavbarComponent.jsx
@@ -97,7 +97,7 @@ export default function PrimarySearchAppBar() {
               variant="h4"
               fontWeight="initial"
             >
-              {user.given_name}
+              {user.nickname}
             </Typography>
           </Button>
         ) : (
